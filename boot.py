1fe906d43096a4fff4636ea243a3ab205e74277d
Sebastian Roll <sebastian.roll@webstep.no>
2019-06-18 09:56:55 +0200

    Add boot.py

---
+++ boot.py
@@ -0,0 +1,41 @@
+
+
+def do_connect(ssid='kubernetes lifeline', pwd='workwork'):
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(ssid, pwd)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+
+
+
+
+def do_webrepl():
+
+	import webrepl
+
+	import machine
+
+	webrepl.start()
+
+	pwd = machine.unique_id()
+
+	# or, start with a specific password
+
+	webrepl.start(password=pwd)
+
+
+
+def no_debug():
+
+    import esp
+
+    # this can be run from the REPL as well
+
+    esp.osdebug(None)
+
